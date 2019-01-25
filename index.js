959eb46e45d2518f3e9f28c5319779037fd8794f
Good luck <2924456233@qq.com>
2019-01-26 03:11:21 +0800

    inish

--- index.js
+++ index.js
@@ -44,3 +44,9 @@
     };
     window && (window.ScrollTopCheck = ScrollTopCheck)
 })();
+
+//  说明：使用方法，直接在window上调用ScrollTopCheck函数或者new ScrollTopCheck(),两个必穿参数，
+//  第一个是容器，比如windo或者div，第二个为需要隐藏的标签，暂且都为必传。
+//  暂为适配移动端。
+//  写成构造函数的形式，感觉之后如果增加逻辑比较好扩展一些，条理也更加清楚，
+//  放在一个自执行函数里，避免了过多的变量造成全局的污染
